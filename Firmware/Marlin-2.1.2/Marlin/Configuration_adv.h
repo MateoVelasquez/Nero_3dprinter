10c3620743d671d1f00934bd81952e525210b432
MateoVelasquez <mateo10velasquez@hotmail.com>
2023-03-24 21:44:45 -0500

    Modificado parametro de nivelacion luego de G28

--- Firmware/Marlin-2.1.2/Marlin/Configuration_adv.h
+++ Firmware/Marlin-2.1.2/Marlin/Configuration_adv.h
@@ -329,7 +329,7 @@
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
   #define THERMAL_PROTECTION_BED_PERIOD        40 // Seconds
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     8 // Degrees Celsius
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     10 // Degrees Celsius
 
   /**
    * As described above, except for the bed (M140/M190/M303).
